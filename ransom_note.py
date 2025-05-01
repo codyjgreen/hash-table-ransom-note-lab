0964668d24511e31a5dc99a9a29f6abc4d15b80a
Cody Green <codyjeromegreen@gmail.com>
2025-05-01 10:32:12 -0700

    solution

--- ransom_note.py
+++ ransom_note.py
@@ -10,4 +10,19 @@ def can_construct(ransomNote: str, magazine: str) -> bool:
     Returns:
         bool: True if ransomNote can be constructed, False otherwise.
     """
-    pass  # TODO: Implement this function
+    # Create a dictionary to count characters manually
+    magazine_counts = {}
+
+    for char in magazine:
+        if char in magazine_counts:
+            magazine_counts[char] += 1
+        else:
+            magazine_counts[char] = 1
+
+    for char in ransomNote:
+        if char not in magazine_counts or magazine_counts[char] == 0:
+            return False
+        magazine_counts[char] -= 1
+
+    return True
+
